dc8e18034626e350a96e4dfd990fabef3725e8fd
Stanislav Kotsiievskyi <restaver174@gmail.com>
2018-07-09 15:38:34 +0300

    Исходная версия скрипта

---
+++ dz3.py
@@ -0,0 +1,11 @@
+import sys , random
+
+def  compute(n):
+    i = 0; s = 0
+    while i  <= n:
+        s +=  random.random ()
+        i += 1
+    return s/n
+    
+n = sys.argv [1]
+print  ’average  of %d random  numbers  is %g" % (n, compute(n))
