c2b7d39f0950ad80db3a1d3c6e5c3493ce7c6f31
Stanislav Kotsiievskyi <restaver174@gmail.com>
2018-07-11 13:22:07 +0300

    Теперь кавычки слева и справа одиаковые

--- dz3.py
+++ dz3.py
@@ -8,4 +8,4 @@ def  compute(n):
     return s/n
     
 n = sys.argv [1]
-print  ’average  of %d random  numbers  is %g" % (n, compute(n))
+print  "average  of %d random  numbers  is %g" % (n, compute(n))
