8168422e1c66e865215fd999a4b9ca86dd033939
Stanislav Kotsiievskyi <restaver174@gmail.com>
2018-07-12 11:20:35 +0300

    Финальная версия 4-го задания

--- dz4.py
+++ dz4.py
@@ -2,10 +2,31 @@ import sys, math
 
 def argv_ln(arg):
     x = float(arg)
-    if arg <= 0:
-        print("%g is illegal"%arg)
+    if x <= 0:
+        print("%g is illegal"%x)
     else:
-        print("ln(%g) = %f"%(arg,math.log(arg)))
+        print("ln(%g) = %f"%(x,math.log(x)))
     return
 
-argv_ln(12.01)
+print("first loop:")
+for r in sys.argv[1:]:
+    argv_ln(r)
+
+print("\nsecond loop:")
+for i in range(1, len(sys.argv)):
+    argv_ln(sys.argv[i])
+
+print("\nthird loop:")
+i = 1
+while i < len(sys.argv):
+    argv_ln(sys.argv[i])
+    i+=1
+
+print("\nfourth loop:")
+i = 1
+while 1:
+    try:
+        argv_ln(sys.argv[i])
+    except IndexError:
+        break
+    i+=1
