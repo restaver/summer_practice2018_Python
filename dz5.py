9ef5de9abb34275286dead1fa9b817560a418a6e
Stanislav Kotsiievskyi <restaver174@gmail.com>
2018-07-13 08:36:52 +0300

    Финальная версия

--- dz5.py
+++ dz5.py
@@ -1,14 +1,12 @@
 import sys , math
 
 try:
-    infilename = sys.argv [1]
-    outfilename = sys.argv [2]
+    filename = sys.argv [1]
 except:
-    print "Usage:", sys.argv[0], "infile  outfile"
+    print "Usage:", sys.argv[0], "outfile"
     sys.exit (1)
 
-ifile = open(infilename , 'r')
-ofile = open(outfilename , 'w')
+ofile = open(filename , 'w')
 
 def  myfunc(y):
     if y  >= 0.0:
@@ -16,12 +14,12 @@ def  myfunc(y):
     else:
         return  0.0
 
-for  line in ifile:
-    pair = line.split ()
-    x = float(pair [0])
-    y = float(pair [1])
+i = 2
+while i+1 < len(sys.argv):
+    x = float(sys.argv[i])
+    y = float(sys.argv[i+1])
     fy = myfunc(y)
     ofile.write('%g %12.5e\n' % (x,fy))
+    i+=2
 
-ifile.close()
 ofile.close()
