61df9c323a627a05a3ff09210d154926ec6f8758
Stanislav Kotsiievskyi <restaver174@gmail.com>
2018-07-13 08:54:06 +0300

    Предфинальная версия

--- dz6.py
+++ dz6.py
@@ -1,27 +1,21 @@
 import sys , math
  
- try:
-     infilename = sys.argv [1]
-     outfilename = sys.argv [2]
- except:
-     print "Usage:", sys.argv[0], "infile  outfile"
-     sys.exit (1)
+try:
+    infilename = sys.argv [1]
+    outfilename = sys.argv [2]
+except:
+    print "Usage:", sys.argv[0], "infile  outfile"
+    sys.exit (1)
  
 ifile = open(infilename , 'r')
 ofile = open(outfilename , 'w')
- 
- def  myfunc(y):
-     if y  >= 0.0:
-         return y**5* math.exp(-y)
-     else:
-         return  0.0
- 
- for  line in ifile:
-     pair = line.split ()
-     x = float(pair [0])
-     y = float(pair [1])
-     fy = myfunc(y)
-     ofile.write('%g %12.5e\n' % (x,fy))
- 
- ifile.close()
- ofile.close()
+  
+for line in ifile:
+    s = 0; i = 0
+    l = line.split ()
+    for x in l:
+        s+=float(x)
+        i+=1
+    ofile.write(line +  ' average: %12.6f\n' % (s/i))
+ifile.close()
+ofile.close()
